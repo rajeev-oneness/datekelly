3b530936b22225dc30148fc4ffc5880ba5bb5420
onenesstechs <arup.panda@onenesstechs.in>
2022-04-13 11:46:21 +0530

    Design changes

--- resources/views/front/mens/premium-picture.blade.php
+++ resources/views/front/mens/premium-picture.blade.php
@@ -69,7 +69,7 @@
 <!-- Modal -->
 <div class="modal fade" id="premiumPictureModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
+        <div class="modal-content" style="width:auto">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
